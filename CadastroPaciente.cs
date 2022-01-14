bdd7f7bc0fd798d2341befcc192dce7750d4cc1b
furlon01 <51270822+furlon01@users.noreply.github.com>
2022-01-14 11:57:51 -0300

    Add files via upload

---
+++ CadastroPaciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public class CadastroPaciente
+    {
+        conexao conexao = new conexao();
+        SqlCommand cmd = new SqlCommand();
+        public string mensagem = "";
+
+        public CadastroPaciente() 
+        {
+            cmd.CommandText = "";
+
+            //cmd.Parameters.AddWithValue("",);
+
+            try
+            {
+                cmd.Connection = conexao.conectar();
+                cmd.ExecuteNonQuery();
+                conexao.desconectar();
+                this.mensagem = "Cadastrado com Sucesso!!";
+            }
+            catch (SqlException e) 
+            {
+                this.mensagem = "Erro ao tentar se conectar com o Banco de Dados!!";
+            }
+        }
+    }
+}
---
+++ conexao.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        //construtor
+        public conexao()
+        {
+            con.ConnectionString = "Data Source=LAPTOP-JSJP7MEP;Initial Catalog=Trabalho-Final;Integrated Security=True";
+        }
+        //Metodo Conectar
+        public SqlConnection conectar() 
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+
+            return con;
+        }
+        //Metodo desconectar
+        public void desconectar() 
+        { 
+            if(con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        } 
+
+    }
+}
